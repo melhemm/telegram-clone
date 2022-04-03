e3db5bfa34d1aadcb85ea39a69a1ed5b867e7b6d
melhemm <jamalmulhemm@gmail.com>
2022-04-04 01:03:45 +0300

    create room and send messages

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -5,6 +5,21 @@ class RoomsController < ApplicationController
     @room = Room.new
     @rooms = Room.public_rooms
     @users = User.all_except(current_user)
+
+    render 'index'
+  end
+
+  def show
+    @single_room = Room.find(params[:id])
+
+    @room = Room.new
+    @rooms = Room.public_rooms
+    @users = User.all_except(current_user)
+
+    @message = Message.new
+    @messages = @single_room.messages.order(created_at: :asc)
+
+    render 'index'
   end
 
   def create
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_04_03_195702) do
+ActiveRecord::Schema.define(version: 2022_04_03_212433) do
+
+  create_table "messages", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.integer "room_id", null: false
+    t.text "body"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["room_id"], name: "index_messages_on_room_id"
+    t.index ["user_id"], name: "index_messages_on_user_id"
+  end
 
   create_table "rooms", force: :cascade do |t|
     t.string "name"
@@ -31,4 +41,6 @@ ActiveRecord::Schema.define(version: 2022_04_03_195702) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "messages", "rooms"
+  add_foreign_key "messages", "users"
 end
