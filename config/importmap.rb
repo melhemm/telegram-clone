b411f3d38fd9bee8832fa45068ed18211e458aa1
melhemm <jamalmulhemm@gmail.com>
2022-04-09 00:33:42 +0300

    stimulus

---
+++ config/importmap.rb
@@ -0,0 +1,6 @@
+# Pin npm packages by running ./bin/importmap
+
+pin "application", preload: true
+pin "stimulus-use", to: "https://ga.jspm.io/npm:stimulus-use@0.41.0/dist/index.js"
+pin "hotkeys-js", to: "https://ga.jspm.io/npm:hotkeys-js@3.8.8/dist/hotkeys.esm.js"
+pin "stimulus", to: "https://ga.jspm.io/npm:stimulus@2.0.0/dist/stimulus.umd.js"
